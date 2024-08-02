3948bf29521a932fc217a0f70c642772df60d55a
NunoMBatista <118256333+NunoMBatista@users.noreply.github.com>
2024-08-02 13:18:01 +0100

    range max prefix

--- DataStructures/SegmentTree/MaxPrefixSum.cpp
+++ DataStructures/SegmentTree/MaxPrefixSum.cpp
@@ -4,13 +4,20 @@ using namespace std;
  
 typedef long long ll;
  
-#define op(l, r) (l + r);
-#define DEFAULTVALUE 0
+
 const ll inf = 1e9;
+#define DEFAULTVALUE -inf
+
+//#define op(l, r) max(l, r+l)
+pair<ll, ll> op(pair<ll, ll> l, pair<ll, ll> r){
+    return make_pair((max(l.first, r.first + l.second)), (l.second + r.second));
+}
+
 struct Node {
     Node *l = 0, *r = 0;
     ll lo, hi, mset = inf, madd = 0;
-    ll val = DEFAULTVALUE;
+    pair<ll, ll> val = {DEFAULTVALUE, 0};
+
     Node(ll lo,ll hi):lo(lo),hi(hi){} // Large interval of -inf
     Node(vector<int>& v, ll lo, ll hi) : lo(lo), hi(hi) {
         if (lo + 1 < hi) {
@@ -18,18 +25,34 @@ struct Node {
             l = new Node(v, lo, mid);
             r = new Node(v, mid, hi);
             val = op(l->val, r->val);
+            //cout << lo << " " << hi << " " <<  val << endl;
         }
-        else val = v[lo];
+        else {
+            val = {v[lo], v[lo]};
+            // cout << val << endl;
+        }
     }
-    ll query(ll L, ll R) {
-        if (R <= lo || hi <= L) return 0;
+    pair<ll, ll> query(ll L, ll R) {
+        if (R <= lo){
+            return make_pair(DEFAULTVALUE, 0);
+        }else if (hi <= L){
+            return make_pair(DEFAULTVALUE, 0);
+        }
         if (L <= lo && hi <= R) return val;
+        // cout << "prepush: " << lo << " " << hi << " " <<  val << endl;
         push();
+        // cout << "pospush: " << lo << " " << hi << " " <<  val << endl;
+        // cout << "\n\n\n";
+
         return op(l->query(L, R), r->query(L, R));
     }
     void set(ll L, ll R, ll x) {
         if (R <= lo || hi <= L) return;
-        if (L <= lo && hi <= R) mset = val = x, madd = 0;
+        if (L <= lo && hi <= R){
+            mset = val.first = x, madd = 0;
+            val.second -= val.second;
+            val.second += x;
+        }
         else {
             push(), l->set(L, R, x), r->set(L, R, x);
             val = op(l->val, r->val);
@@ -40,7 +63,7 @@ struct Node {
         if (L <= lo && hi <= R) {
             if (mset != inf) mset += x;
             else madd += x;
-            val += x;
+            val.first += x;
         }
         else {
             push(), l->add(L, R, x), r->add(L, R, x);
@@ -59,24 +82,40 @@ struct Node {
     }
 };
 
-void solve(){
-    int type, k, u, n, q; 
-    cin >> n >> q; 
 
+void solve(){
+    int type, a, b, n, q;
+    cin >> n >> q;
+    
     vector<int> v(n);
     for(auto &i: v) cin >> i;
 
-    Node *root = new Node(v, 0, n);
+    auto* root = new Node(v, 0, n);
+    // for(int i = 0; i < n; i++){
+    //     for(int j = i; j < n; j++){
+    //         cout << (root->query(i, j+1)).first << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl << endl << endl;
 
     while(q--){
-        cin >> type >> k >> u; 
+        cin >> type >> a >> b;
+
         if(type == 1){
-            root->set(k-1, k, u);
+            root->set(a-1, a, b);
             continue;
         }
-        cout << root->query(k-1, u) << endl;
 
-    }   
+        // cout << type << " " << a << " " << b << endl;
+        cout << max(0LL, root->query(a-1, b).first) << endl;
+        // // for(int i = 0; i < n; i++){
+        // //     cout << root->query(i, i+1)) << " ";
+        // // }
+        // cout << endl << endl;
+    }
+
+
 }
 int main()
 {
